19703685fcdcd3df6ed79d8d5d638a8a96ed0823
breteo <44599374+breteo@users.noreply.github.com>
2023-10-03 19:21:31 -0700

    finished form validation

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 import './style.css'
+
+const signup_submit = document.getElementsByClassName("submit-button");
+
+const emailFormat = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+const name = ["First Name cannot be empty", "Last Name cannot be empty", "Looks like this is not an email", "Password cannot be empty"];
+
+const text = document.querySelectorAll(".text");
+for(let i=0;i<text.length;i++){
+    text[i].classList.remove("text");
+}
+
+signup_submit[0].addEventListener("click", () => {
+    const input = document.querySelectorAll("input");
+    console.log(text);
+    for(let i = 0; i < input.length; i++) {
+        for(let i = 0; i < text.length; i++) {
+           if(input[i].value === "") {
+            input[i].classList.add("error");
+            text[i].classList.add("text");
+            text[i].innerHTML = name[i];
+        } else {
+            input[i].classList.remove("error");
+            text[i].classList.add("text");
+            text[i].innerHTML = "";
+        } 
+            
+        if (i == 2 &&  !input[2].value.match(emailFormat)){
+            input[i].classList.add("error");
+            text[i].classList.add("text");
+            text[i].innerHTML = name[i];
+        }    
+    }
+  }
+});
